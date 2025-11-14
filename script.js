0dea7c402bb608b057b70b9fab0acf335840f67f
RANAK GHOSH <110456420+Ranakghosh7@users.noreply.github.com>
2025-11-14 21:39:16 +0100

    Add particle animation effect to the canvas

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
 alert("It is a non-profit E-Commerce website specially made to support and promote women empowerment. For more information see the About page of this website");
+
+<script>
+const canvas = document.getElementById("magicCursor");
+const ctx = canvas.getContext("2d");
+let particles = [];
+
+canvas.style.position = "fixed";
+canvas.style.top = 0;
+canvas.style.left = 0;
+canvas.style.pointerEvents = "none";
+canvas.style.zIndex = 9999;
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resize();
+window.addEventListener("resize", resize);
+
+class Particle {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.size = Math.random() * 6 + 2;
+    this.speedX = (Math.random() - 0.5) * 4;
+    this.speedY = (Math.random() - 0.5) * 4;
+    this.color = color;
+    this.life = 100;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.size *= 0.96;
+    this.life--;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+
+function randomColor() {
+  return `hsl(${Math.random() * 360}, 100%, 70%)`;
+}
+
+window.addEventListener("mousemove", (e) => {
+  for (let i = 0; i < 5; i++) {
+    particles.push(new Particle(e.clientX, e.clientY, randomColor()));
+  }
+});
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particles.forEach((p, i) => {
+    p.update();
+    p.draw();
+    if (p.life <= 0 || p.size < 0.5) particles.splice(i, 1);
+  });
+  requestAnimationFrame(animate);
+}
+animate();
+</script>
